1456392e87d286d719a0fd1784028baec4bdb7b5
drewjya <andre8.aw6@gmail.com>
2025-08-05 11:10:10 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia")
+const app = new Elysia().get("/", () => "Hello Elysia");
 
-app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
+const testendpoint = new Elysia().get("/test", () => "Hello Test");
+
+app
+  .use(testendpoint)
+  .listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
